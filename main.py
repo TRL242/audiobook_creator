e88de52e6fc9f768aa766441c6623cffad14e6bf
trl242 <royaurallockhart@gmail.com>
2021-04-11 10:11:17 -0700

    plays audio from pdf in proper speed about to have it create mp3

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-install pyttsx3
-install PyPDF2
+import PyPDF2
+import pyttsx3
+
+
+with open('book/Bridgewater Associates Ray Dalio - Principles.pdf', 'rb') as book:
+
+    reader = PyPDF2.PdfFileReader(book)
+
+    audio_reader = pyttsx3.init()
+    audio_reader.setProperty("rate", 170)
+
+    for page in range(reader.numPages):
+        next_page = reader.getPage(page)
+        content = next_page.extractText()
+        #to listening from python
+        # audio_reader.say(content)
+        audio_reader.save_to_file(content, "myaudiobook.mp3")
+        audio_reader.runAndWait()
 
